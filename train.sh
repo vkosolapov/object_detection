0a3171817696b3bdccbcc89bab26f8b4618ed67a
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-17 23:07:23 +0300

    Change epochs count

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 3 --data data/AFO/dataset.yaml --weights yolov5s.pt
+python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 100 --data data/AFO/dataset.yaml --weights yolov5s.pt
