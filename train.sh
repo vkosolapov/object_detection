a9e3713a13c7df2a86a0952e78d26f3beb10235f
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-18 22:11:14 +0300

    300 epochs

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 150 --data data/AFO/dataset.yaml --weights yolov5s.pt --project object_detection_ --name 001_baseline
+python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 300 --data data/AFO/dataset.yaml --weights yolov5s.pt --project object_detection_ --name 002_300_epochs
