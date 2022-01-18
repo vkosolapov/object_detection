f4a17e57137bab82ef3a6a9a51cd47c474587082
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-18 10:15:08 +0300

    Add WandB

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 100 --data data/AFO/dataset.yaml --weights yolov5s.pt
+python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 300 --data data/AFO/dataset.yaml --weights yolov5s.pt --project afo_yolo --run baseline
