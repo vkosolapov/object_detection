7d4ec72afd6fd9941fdbadede9948f7620cbdbdf
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-18 13:42:40 +0300

    Fix run name for WandB

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 150 --data data/AFO/dataset.yaml --weights yolov5s.pt --project afo_yolo --run baseline
+python object_detection/yolov5/train.py --img 640 --batch 16 --epochs 150 --data data/AFO/dataset.yaml --weights yolov5s.pt --project afo_yolo --name baseline
