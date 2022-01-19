f01e782e8fe1650ce2e6c12b9e1f98a4db5bfece
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-19 15:54:59 +0300

    image size 960

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python object_detection/yolov5/train.py --img 640 --batch 32 --epochs 300 --data data/AFO/dataset.yaml --weights yolov5s.pt --project object_detection_ --name 003_batch_32
+python object_detection/yolov5/train.py --img 960 --batch 16 --epochs 300 --data data/AFO/dataset.yaml --weights yolov5s.pt --project object_detection_ --name 004_image_960
