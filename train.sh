73b7c1e4803cc5510a70268da60e3e4c8a5bf1eb
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-22 15:37:52 +0300

    Soft loss

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 python object_detection/yolov5/train.py \
     --img 1280 --batch 8 \
     --epochs 500 --patience 25 \
-    --label-smoothing 0.05 \
+    --label-smoothing 0.01 \
     --data data/AFO/dataset.yaml \
     --weights yolov5s.pt \
     --hyp object_detection/yolov5/data/hyps/hyp.scratch.yaml \
     --workers 12 \
-    --project object_detection_ --name 009_loss_weights
+    --project object_detection_ --name 010_soft_loss
