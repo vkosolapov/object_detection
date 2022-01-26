42b607eb8ca03706af6ba4ab6308b24a870560e6
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-01-26 17:59:09 +0300

    Remove augs from centernet dataset

--- object_detection/data/dataset.py
+++ object_detection/data/dataset.py
@@ -88,9 +88,7 @@ class CenternetDataset(Dataset):
     def __getitem__(self, index):
         index = index % self.length
 
-        image, box = self.get_random_data(
-            self.annotation_lines[index], self.input_shape, random=self.train
-        )
+        image, box = self.get_data(self.annotation_lines[index], self.input_shape)
         batch_hm = np.zeros(
             (self.output_shape[0], self.output_shape[1], self.num_classes),
             dtype=np.float32,
@@ -144,15 +142,8 @@ class CenternetDataset(Dataset):
     def rand(self, a=0, b=1):
         return np.random.rand() * (b - a) + a
 
-    def get_random_data(
-        self,
-        annotation_line,
-        input_shape,
-        jitter=0.3,
-        hue=0.1,
-        sat=1.5,
-        val=1.5,
-        random=True,
+    def get_data(
+        self, annotation_line, input_shape,
     ):
         line = annotation_line.split()
         image = Image.open(line[0])
@@ -161,82 +152,26 @@ class CenternetDataset(Dataset):
         h, w = input_shape
         box = np.array([np.array(list(map(int, box.split(",")))) for box in line[1:]])
 
-        if not random:
-            scale = min(w / iw, h / ih)
-            nw = int(iw * scale)
-            nh = int(ih * scale)
-            dx = (w - nw) // 2
-            dy = (h - nh) // 2
+        scale = min(w / iw, h / ih)
+        nw = int(iw * scale)
+        nh = int(ih * scale)
+        dx = (w - nw) // 2
+        dy = (h - nh) // 2
 
-            image = image.resize((nw, nh), Image.BICUBIC)
-            new_image = Image.new("RGB", (w, h), (128, 128, 128))
-            new_image.paste(image, (dx, dy))
-            image_data = np.array(new_image, np.float32)
-
-            if len(box) > 0:
-                np.random.shuffle(box)
-                box[:, [0, 2]] = box[:, [0, 2]] * nw / iw + dx
-                box[:, [1, 3]] = box[:, [1, 3]] * nh / ih + dy
-                box[:, 0:2][box[:, 0:2] < 0] = 0
-                box[:, 2][box[:, 2] > w] = w
-                box[:, 3][box[:, 3] > h] = h
-                box_w = box[:, 2] - box[:, 0]
-                box_h = box[:, 3] - box[:, 1]
-                box = box[np.logical_and(box_w > 1, box_h > 1)]  # discard invalid box
-
-            return image_data, box
-
-        new_ar = (
-            w
-            / h
-            * self.rand(1 - jitter, 1 + jitter)
-            / self.rand(1 - jitter, 1 + jitter)
-        )
-        scale = self.rand(0.25, 2)
-        if new_ar < 1:
-            nh = int(scale * h)
-            nw = int(nh * new_ar)
-        else:
-            nw = int(scale * w)
-            nh = int(nw / new_ar)
         image = image.resize((nw, nh), Image.BICUBIC)
-
-        dx = int(self.rand(0, w - nw))
-        dy = int(self.rand(0, h - nh))
         new_image = Image.new("RGB", (w, h), (128, 128, 128))
         new_image.paste(image, (dx, dy))
-        image = new_image
-
-        flip = self.rand() < 0.5
-        if flip:
-            image = image.transpose(Image.FLIP_LEFT_RIGHT)
-
-        hue = self.rand(-hue, hue)
-        sat = self.rand(1, sat) if self.rand() < 0.5 else 1 / self.rand(1, sat)
-        val = self.rand(1, val) if self.rand() < 0.5 else 1 / self.rand(1, val)
-        x = cv2.cvtColor(np.array(image, np.float32) / 255, cv2.COLOR_RGB2HSV)
-        x[..., 0] += hue * 360
-        x[..., 0][x[..., 0] > 1] -= 1
-        x[..., 0][x[..., 0] < 0] += 1
-        x[..., 1] *= sat
-        x[..., 2] *= val
-        x[x[:, :, 0] > 360, 0] = 360
-        x[:, :, 1:][x[:, :, 1:] > 1] = 1
-        x[x < 0] = 0
-        image_data = cv2.cvtColor(x, cv2.COLOR_HSV2RGB) * 255
+        image_data = np.array(new_image, np.float32)
 
         if len(box) > 0:
             np.random.shuffle(box)
             box[:, [0, 2]] = box[:, [0, 2]] * nw / iw + dx
             box[:, [1, 3]] = box[:, [1, 3]] * nh / ih + dy
-            if flip:
-                box[:, [0, 2]] = w - box[:, [2, 0]]
             box[:, 0:2][box[:, 0:2] < 0] = 0
             box[:, 2][box[:, 2] > w] = w
             box[:, 3][box[:, 3] > h] = h
             box_w = box[:, 2] - box[:, 0]
             box_h = box[:, 3] - box[:, 1]
-            box = box[np.logical_and(box_w > 1, box_h > 1)]
+            box = box[np.logical_and(box_w > 1, box_h > 1)]  # discard invalid box
 
         return image_data, box
-
