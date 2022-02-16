a81fd53ac416f1e8aa44e64674cf1fb1d464bd00
Vladimir Kosolapov <v.kosolapov@hotmail.com>
2022-02-16 12:25:31 +0300

    Fix YOLOv5

--- train.sh
+++ train.sh
@@ -2,9 +2,8 @@ python object_detection/yolov5/train.py \
     --img 1280 --batch 8 \
     --epochs 500 --patience 100 \
     --label-smoothing 0.0 \
-    --optimizer Ranger \
     --data data/AFO/dataset.yaml \
-    --weights yolov5s.pt \
     --hyp object_detection/yolov5/data/hyps/hyp.scratch.yaml \
     --workers 16 \
-    --project object_detection_ --name 014_no_aug_loss_change
+    --project object_detection_ --name 027_yolov5_from_scratch
+    # --weights yolov5s.pt \
